d3abacc985dd1c8b52f828d1500ef1772941a4fd
Vali Manea <118940094+valimanea@users.noreply.github.com>
2023-03-01 22:36:47 +0000

    function to add a manager created

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const inquirer =require ("inquirer");
 const path = require("path");
 const fs = require("fs");
+const chalk = require("chalk");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,4 +13,70 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const team = [];
 
+const addManagerData = () =>{
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the team manager’s name: ' + chalk.italic("(1-30 characters)"), 
+            validate: validateName
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the team manager’s employee ID: ' + chalk.italic("(numbers only)"),
+            validate: validateNumber
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the team manager’s email address: ',
+            validate: validateEmail
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter the team manager’s office number:' + chalk.italic("(numbers only)") ,
+            validate: validateNumber
+        }
+    ])
+    .then(dataInput => {
+        console.log(dataInput);
+        const manager = new Manager (dataInput.name, dataInput.id, dataInput.email, dataInput.officeNumber);
+        team.push(manager);
+        console.log(team);
+    })
+}
+
+
+function validateName(name) {
+    if (name.length <= 30 && name.length > 0) {
+        return true;
+    } 
+    return chalk.red('Please insert a name of 1 to 30 characters!')
+}
+
+function validateNumber(num) {
+    if (/^[0-9]+$/.test(num)) {
+        return true;
+    } 
+    return chalk.red('Please enter a numeric value!')
+}
+
+function validateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+    }
+    return chalk.red('Please enter a valid email address!')
+}
+
+function validateGithub(github) {
+    if (/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(github)) {
+        return true;
+    } 
+    return chalk.red('Please enter a valid GitHub username!')
+}
+
+addManagerData();
